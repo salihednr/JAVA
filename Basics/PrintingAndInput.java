04e3a5daf15f7d5b84a92ce9b3ff90144ac7dcae
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-20 00:26:39 +0530

    Add files via upload

---
+++ Basics/PrintingAndInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PrintingAndInput{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String name1=input.next();
+        System.out.println(name1);
+    }
+}
+
+
+
